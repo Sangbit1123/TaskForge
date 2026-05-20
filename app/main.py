de6bd91109ff238cb07175ff8c4897b71e94023b
Prama <prama@example.com>
2026-05-20 13:32:40 +0530

    fixed table creation

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI, Depends,HTTPException
+from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
-from fastapi.security import OAuth2PasswordBearer,OAuth2PasswordRequestForm
-from sqlalchemy.orm import Session
-from app.database import SessionLocal, engine, Base
+from fastapi.security import OAuth2PasswordBearer
+
+from app.database import engine, Base
 
 from app.routes.auth_routes import router as auth_router
 from app.routes.project_routes import router as project_router
@@ -10,6 +10,7 @@ from app.routes.task_routes import router as task_router
 from app.routes.dashboard_routes import router as dashboard_router
 from app.routes.user_routes import router as user_router
 from app.routes.frontend_routes import router as frontend_router
+
 from app.models import *
 
 oauth2_scheme = OAuth2PasswordBearer(
@@ -17,6 +18,11 @@ oauth2_scheme = OAuth2PasswordBearer(
 )
 
 app = FastAPI()
+
+# Create all tables
+Base.metadata.create_all(bind=engine)
+
+# Include routers
 app.include_router(auth_router)
 app.include_router(project_router)
 app.include_router(task_router)
@@ -24,11 +30,8 @@ app.include_router(dashboard_router)
 app.include_router(user_router)
 app.include_router(frontend_router)
 
-Base.metadata.create_all(bind=engine)
-
 @app.get("/")
 def root():
-
     return RedirectResponse(
         url="/login-page"
     )
